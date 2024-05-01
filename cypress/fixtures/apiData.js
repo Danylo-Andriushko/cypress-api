81fb128b7fa7fa4509da234ae35d624283fa234e
Danylo Andriushko <danylo_andriushko@epam.com>
2024-05-01 22:47:39 +0300

    ci/cd test

--- cypress/fixtures/apiData.js
+++ cypress/fixtures/apiData.js
@@ -3,7 +3,7 @@ export class ApiData  {
         phonePrices: [360.0, 820.0, 650.0, 800.0, 790.0, 320.0, 700.0, 790.0, 790.0],
         phoneTitles: ["Samsung galaxy s6", "Nokia lumia 1520", "Nexus 6", "Samsung galaxy s7", "Iphone 6 32gb", "Sony xperia z5", "HTC One M9"],
         entriesAllowMethods: "HEAD, GET, OPTIONS",
-        bycatAllowMethods: "POST, OPTIONS",
-        viewtAllowMethods: "POST, OPTIONS"
+        bycatAllowMethods: "OPTIONS, POST",
+        viewtAllowMethods: "OPTIONS, POST"
     }
 }
